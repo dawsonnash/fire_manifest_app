4f6ee9c878efb1783deebb53b4b2574e8a4aa94e
dawsonnash <dawsonak85@gmail.com>
2024-10-09 21:47:26 -0800

    Added 'totalCrewWeight' attribute to Crew Class and functions to update it upon adding crewmembers/gear

--- lib/Data/crew.dart
+++ lib/Data/crew.dart
@@ -4,21 +4,41 @@ import 'gear.dart';
 class Crew {
   List<CrewMember> crewMembers = [];
   List<Gear> gear = [];
+  double totalCrewWeight = 0.0;
+
+  void updateTotalCrewWeight() {
+    double crewWeight = 0.0;
+    for (var member in crewMembers) {
+      crewWeight += member.flightWeight;
+    }
+
+    double gearWeight = 0.0;
+    for (var gearItem in gear) {
+      gearWeight += gearItem.weight;
+    }
+
+    totalCrewWeight = crewWeight + gearWeight;
+  }
+
 
   void addCrewMember(CrewMember member) {
     crewMembers.add(member);
+    updateTotalCrewWeight();
   }
 
   void removeCrewMember(CrewMember member){
     crewMembers.remove(member);
+    updateTotalCrewWeight();
   }
 
   void removeGear(Gear gearItem){
     gear.remove(gearItem);
+    updateTotalCrewWeight();
   }
 
   void addGear(Gear gearItem) {
     gear.add(gearItem);
+    updateTotalCrewWeight();
   }
 
   // For LogCat testing purposes
