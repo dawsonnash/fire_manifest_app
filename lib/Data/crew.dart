b286598d110d9579df061ffee014b7f400922e27
bnbrown2 <benbrown4784@gmail.com>
2024-10-13 18:14:05 -0800

    changed updateTotalCrewWeight to use values from Hive rather than in memory

--- lib/Data/crew.dart
+++ lib/Data/crew.dart
@@ -7,31 +7,32 @@ class Crew {
   List<Gear> gear = [];
   double totalCrewWeight = 0.0;
 
+  late final Box<CrewMember> crewmemberBox;
+  late final Box<Gear> gearBox;
+
   void updateTotalCrewWeight() {
     double crewWeight = 0.0;
-    for (var member in crewMembers) {
+    for (var member in crewmemberBox.values) {
       crewWeight += member.flightWeight;
     }
 
     double gearWeight = 0.0;
-    for (var gearItem in gear) {
+    for (var gearItem in gearBox.values) {
       gearWeight += gearItem.weight;
     }
 
     totalCrewWeight = crewWeight + gearWeight;
+    print(totalCrewWeight);
   }
 
 
   void addCrewMember(CrewMember member) {
-    var crewmemberBox = Hive.box<CrewMember>('crewmemberBox');
     crewMembers.add(member); // add to in memory as well
     crewmemberBox.add(member); // save to hive memory
     updateTotalCrewWeight();
   }
 
   void removeCrewMember(CrewMember member){
-    var crewmemberBox = Hive.box<CrewMember>('crewmemberBox');
-
     final keyToRemove = crewmemberBox.keys.firstWhere( // find which Hive key we want to remove
           (key) => crewmemberBox.get(key) == member,
       orElse: () => null,
@@ -44,8 +45,6 @@ class Crew {
   }
 
   void removeGear(Gear gearItem){
-    var gearBox = Hive.box<Gear>('gearBox');
-
     final keyToRemove = gearBox.keys.firstWhere( // find which Hive key we want to remove
           (key) => gearBox.get(key) == gearItem,
       orElse: () => null,
@@ -58,7 +57,6 @@ class Crew {
   }
 
   void addGear(Gear gearItem) {
-    var gearBox = Hive.box<Gear>('gearBox');
     gear.add(gearItem); // added to in-memory as well
     gearBox.add(gearItem); // save to hive memory
     updateTotalCrewWeight();
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'edit_crew.dart';
-import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'Data/gear.dart';
 import 'Data/crewmember.dart';
