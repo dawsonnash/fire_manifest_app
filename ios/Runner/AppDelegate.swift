7b35190df78c035ee59c0607b5f393ab9bd8bb7a
Dawson Nash <dawsonak85@gmail.com>
2025-03-17 10:18:26 -0800

    Updated iOS Share Extension and Open In handling

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,7 @@
 import Flutter
 import UIKit
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
@@ -9,5 +9,22 @@ import UIKit
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  // ✅ Add this function to handle "Open In" file URLs
+  override func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+  ) -> Bool {
+    if url.pathExtension == "json" {
+      let userDefaults = UserDefaults.standard
+      userDefaults.set(url.absoluteString, forKey: "sharedJsonFile")
+      userDefaults.synchronize()
+      
+      NotificationCenter.default.post(name: Notification.Name("importJson"), object: url.absoluteString)
+      return true
+    }
+    return false
   }
 }
